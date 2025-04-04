dc54e9d89c1d6bd6bb1fd0f06f2ec9bb389182d6
shivaskn <shivaskn299@gmail.com>
2025-04-04 13:40:51 +0530

    asdasd

--- components/Blogitem.jsx
+++ components/Blogitem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { assets, blog_data } from "@/Assets/assets";
+import { assets } from "@/Assets/assets";
 import Link from "next/link";
 
 const Blogitem = ({ title, description, category, image, id }) => {
